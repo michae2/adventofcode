7c168534f15cfdd9c9060f24160a2f02688f62fe
Michael Erickson <32038551+michae2@users.noreply.github.com>
2019-02-03 20:41:36 -0800

    fix 2018, day 1, python

--- 2018/01/b.py
+++ 2018/01/b.py
@@ -5,10 +5,10 @@ import sys
 
 changes = map(int, sys.stdin)
 changes = itertools.cycle(changes)
-changes = changes.__iter__()
+changes = iter(changes)
 freqs = set()
 freq = 0
 while freq not in freqs:
     freqs.add(freq)
-    freq += changes.__next__()
+    freq += next(changes)
 print(freq)
