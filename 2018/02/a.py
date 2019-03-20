dbd6c1d61f6d0d0947f048cc8c96399b07af2e4a
Michael Erickson <32038551+michae2@users.noreply.github.com>
2019-03-20 07:35:46 -0700

    use counter in 2018, day 2, part a, python

--- 2018/02/a.py
+++ 2018/02/a.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
+import collections
 import sys
 
 twos = 0
 threes = 0
 for line in sys.stdin:
     box_id = line.rstrip()
-    counts = {}
-    for letter in box_id:
-        counts[letter] = counts.get(letter, 0) + 1
+    counts = collections.Counter(box_id)
     if 2 in counts.values():
         twos += 1
     if 3 in counts.values():
