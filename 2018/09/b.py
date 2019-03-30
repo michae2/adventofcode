7c81f88d2bc8b466d7431f9ef5398c67b6177264
Michael Erickson <32038551+michae2@users.noreply.github.com>
2019-03-30 07:43:58 -0700

    2018, day 9, python

---
+++ 2018/09/b.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import collections
+import re
+
+pattern = r'(\d+) players; last marble is worth (\d+) points'
+players, last = tuple(map(int, re.match(pattern, input()).groups()))
+
+scores = collections.Counter()
+circle = collections.deque()
+for m in range(last * 100 + 1):
+    if m > 0 and m % 23 == 0:
+        circle.rotate(7)
+        scores[m % players] += m + circle.popleft()
+    else:
+        circle.rotate(-2)
+        circle.appendleft(m)
+
+print(scores.most_common(1)[0][1])
